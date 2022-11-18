f6730dfb35a6ebf75a87bb00c942cfcbfc13defa
Elson Otake <elsonotake@yahoo.com.br>
2022-11-18 17:36:50 -0300

    Update Schedule routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'schedule/index'
-  get 'coach/index'
   get 'user/index'
   get 'user/show'
   get 'user/create'
@@ -8,4 +6,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'user#index'
+
+  resources :coach, only: %i[index] do
+    resources :schedule, only: %i[index]
+  end
 end
