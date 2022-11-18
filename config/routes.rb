d5889f494af52f5283cb3b39f4ae7333e22c3127
Elson Otake <elsonotake@yahoo.com.br>
2022-11-18 15:58:53 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'schedule/index'
+  get 'coach/index'
+  get 'user/index'
+  get 'user/show'
+  get 'user/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
