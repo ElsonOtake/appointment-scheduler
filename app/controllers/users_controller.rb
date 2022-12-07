cbf1e1fba508afd1a1b97d91a7700e973971083c
Elson Otake <elsonotake@yahoo.com.br>
2022-12-07 09:25:01 -0300

    Add create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
-  def index
-    @user = User.first
-  end
+  def index; end
 
   def show; end
 
-  def create; end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to user_coaches_path(@user.id)
+    else
+      flash[:error] = 'Error- please try to create an account again.'
+      redirect_to new_session_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
