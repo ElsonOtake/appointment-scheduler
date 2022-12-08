ae8c0b5e82c6591141066f40989f9ca7fc6db88e
Elson Otake <elsonotake@yahoo.com.br>
2022-12-08 14:18:41 -0300

    Fix path to sign in and up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: %i[new create destroy]
   root 'sessions#new'
 
-  resources :users, only: %i[index show create] do
+  resources :users, only: %i[new create] do
     resources :coaches, only: %i[index] do
       resources :schedules, only: %i[index]
     end
