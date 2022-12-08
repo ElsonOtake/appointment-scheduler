4aec137a257dad441393bd54e90db885fdd9e2af
Elson Otake <elsonotake@yahoo.com.br>
2022-12-08 19:05:49 -0300

    Add variables to day of week

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,5 +3,12 @@ class SchedulesController < ApplicationController
     @user = User.find(params[:user_id])
     @coach = Coach.find(params[:coach_id])
     @schedules = @coach.schedules
+    @sunday = @coach.schedules.where(day_of_week: 'Sunday')
+    @monday = @coach.schedules.where(day_of_week: 'Monday')
+    @tuesday = @coach.schedules.where(day_of_week: 'Tuesday')
+    @wednesday = @coach.schedules.where(day_of_week: 'Wednesday')
+    @thursday = @coach.schedules.where(day_of_week: 'Thursday')
+    @friday = @coach.schedules.where(day_of_week: 'Friday')
+    @saturday = @coach.schedules.where(day_of_week: 'Saturday')
   end
 end
